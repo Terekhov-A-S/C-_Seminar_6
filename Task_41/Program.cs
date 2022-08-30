bb24bd82f9e54c9e12f730e564afdccf05e1c47c
KoT <Kotto@student.su>
2022-08-30 19:48:54 +0400

    modif task

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,26 +1,26 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 Console.Clear();
 
-Console.Write("Введите число, которое будет обозначать количество чисел): ");
+Console.Write("Введите число, которое будет обозначать количество чисел: ");
 int m = Convert.ToInt32(Console.ReadLine());
-int[] masNumbers = new int[m];
+int[] arrNumbers = new int[m];
 void InputNumbers(int m)
 {
 for (int i = 0; i < m; i++)
   {
     Console.Write($"Введите {i+1} число: ");
-    masNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    arrNumbers[i] = Convert.ToInt32(Console.ReadLine());
   }
 }
-int Compare(int[] masNumbers)
+int Compare(int[] arrNumbers)
 {
   int count = 0;
-  for (int i = 0; i < masNumbers.Length; i++)
+  for (int i = 0; i < arrNumbers.Length; i++)
   {
-    if(masNumbers[i] > 0 ) count += 1; 
+    if(arrNumbers[i] > 0 ) count += 1; 
   }
   return count;
 }
 InputNumbers(m);
-Console.WriteLine($"Введено чисел больше 0: {Compare(masNumbers)} ");
+Console.WriteLine($"Введено чисел больше 0: {Compare(arrNumbers)} ");
 
