16c9c8668fed2cf832cdddd13b5e834121ebcfa1
KoT <Kotto@student.su>
2022-08-30 19:45:12 +0400

    Added files

---
+++ Task_41/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+Console.Write("Введите число, которое будет обозначать количество чисел): ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] masNumbers = new int[m];
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    masNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+int Compare(int[] masNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < masNumbers.Length; i++)
+  {
+    if(masNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+InputNumbers(m);
+Console.WriteLine($"Введено чисел больше 0: {Compare(masNumbers)} ");
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.Clear();
+
+double[,] ratio = new double[2, 2];
+double[] intersectionPoint = new double[2];
+void InputRatio()
+{
+  for (int i = 0; i < ratio.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты {i+1}-ой прямой\n");
+    for (int j = 0; j < ratio.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      ratio[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+double[] Decision(double[,] ratio)
+{
+  intersectionPoint[0] = (ratio[1,1] - ratio[0,1]) / (ratio[0,0] - ratio[1,0]);
+  intersectionPoint[1] = intersectionPoint[0] * ratio[0,0] + ratio[0,1];
+  return intersectionPoint;
+}
+void OutputResponse(double[,] ratio)
+{
+  if (ratio[0,0] == ratio[1,0] && ratio[0,1] == ratio[1,1]) 
+  {
+    Console.Write($"* Прямые совпадают *\n");
+  }
+  else if (ratio[0,0] == ratio[1,0] && ratio[0,1] != ratio[1,1]) 
+  {
+    Console.Write($"* Прямые параллельны *\n");
+  }
+  else 
+  {
+    Decision(ratio);
+    Console.Write($"* Точка пересечения прямых: ({intersectionPoint[0]}, {intersectionPoint[1]}) *\n");
+  }
+}
+InputRatio();
+OutputResponse(ratio);
+
